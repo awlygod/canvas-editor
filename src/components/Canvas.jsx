85e3b86e8693ffcbef32242e2f9a94442d6b3aeb
Suraj Tripathi <145766997+awlygod@users.noreply.github.com>
2026-03-23 11:57:45 +0530

    Remove comment regarding Canvas styling
    Removed unnecessary comment about styling.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-/* Styling is handled by .cp-canvas-surface in CanvasPage.css */
+
 const Canvas = ({ canvasRef }) => <canvas ref={canvasRef} />;
 
 export default Canvas;
